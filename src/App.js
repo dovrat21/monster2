8362dd476b2f7aeea9de309bd0330cbe1312dfaa
dovrat21 <dovrat.gratziany@gmail.com>
2020-09-23 08:51:15 +0300

    טט

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {SearchBox} from './components/searc-box/search-box.component';
 import './App.css';
 
 class App extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
       string:"dubdub",
       monsters:[],
-      searchField:""
+      searchField:"",
+      title:"",
+      meaningOfLife:47 + this.props.increment
      
      }
      
@@ -19,24 +21,50 @@ class App extends Component{
     fetch("https://jsonplaceholder.typicode.com/users").then(response=>response.json().then(users=>this.setState({monsters:users})));
   }
 
-handleChange = e =>{
-  this.setState({searchField:e.target.value});
   
+handleChange = e =>{
+  this.setState({searchField:e.target.value,
+                 title:e.target.value
+  });
+  
+};
+
+handleClick = () =>{
+  //this.setState({       not synchronize - there is severtal setstate that might interupt. if setstate include this.state we should do something else
+   // meaningOfLife:this.state.meaningOfLife +1
+ // })
+
+ this.setState((prevState,prevProps) => {
+   return {meaningOfLife:prevState.meaningOfLife + prevProps.increment }
+ }, 
+    
+  () =>console.log(this.state.meaningOfLife)
+  )
+ 
+ 
 };
 
 
   render(){
-    const { monsters, searchField} = this.state;
+    const { monsters, searchField,title} = this.state;
     const filteredMonsters = monsters.filter( monster=>
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
    
         return (
           <div className="App">
-            <h1>TEst</h1>
+           
             <h1 className="dovfont">Monster rolex</h1>
+           
             <input className='search-mons' type="search"  placeholder="dov2" onChange={e => this.setState({searchField: e.target.value})} />
-          
-           <SearchBox className="search-mons" placeHolder='search monsters' handleChange={e => this.handleChange} />
+            <h2>{this.state.meaningOfLife}</h2>
+            <button onClick={this.handleClick}>
+              update
+            </button>
+            <div>
+              מחפש:
+        <span>{title}</span>
+            </div>
+           <SearchBox className="search-mons" placeHolder='search monsters' handleChange={this.handleChange} />
             <CardList monsters={filteredMonsters}>{ }</CardList>
               
            
